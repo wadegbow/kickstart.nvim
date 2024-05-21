55259e916efe02bb8a8e5c2bde0749e73891d74a
Wade Bowers <wade.bowers@statenews.com>
2024-05-21 12:30:36 -0400

    mbl as twig, add oil.nvim

--- init.lua
+++ init.lua
@@ -71,6 +71,7 @@ vim.opt.scrolloff = 10
 vim.filetype.add {
   extension = {
     tpl = 'twig',
+    mbl = 'twig',
   },
 }
 
@@ -733,7 +734,12 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
-
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
